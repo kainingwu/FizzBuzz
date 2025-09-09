d9c9c3ffbaea41b793ffa771b434c2f72602d1cf
kainingwu <kainingwu.gz@gmail.com>
2025-09-09 18:17:40 -0400

    reduce

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -2,18 +2,22 @@ package lab1;
 
 public class Reduce {
     public static void main(String[] args) {
-        int n = 100;
+        int n = 200;
         int count = 0;
         count = reduce(n);
         System.out.println(count);
     }
 
     public static int reduce(int n) {
-        int count;
-        if (n % 2 == 0) {
-            count = n / 2 + 1;
-        } else {
-            count = (n - 1) / 2 + 2;
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                count++;
+                n = n / 2;
+            } else {
+                n = n - 1;
+                count++;
+            }
         }
         return count;
     }
